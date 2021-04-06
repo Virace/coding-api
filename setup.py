137082a7502304f9d65bd13178787fe45b0b62fd
Virace <Virace@aliyun.com>
2021-04-06 20:09:57 +0800

    Initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='coding-api',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/Virace/coding-api',
+    license='GNU General Public License v3.0',
+    author='Virace',
+    author_email='Virace@aliyun.com',
+    description='CODING.NET OPEN API'
+)
